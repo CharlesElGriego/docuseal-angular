0ce9d3e47c63ccb4e608527c8e9f3a5c746f9e0c
Carlos Navarro <carlos.navarro@sight-sound.com>
2024-03-21 19:28:13 -0600

    feat: add output events

--- src/builder.component.ts
+++ src/builder.component.ts
@@ -1,117 +1,180 @@
-import { Component, Input, HostListener, HostBinding } from "@angular/core"
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
 
 interface DocusealField {
-  name: string,
-  type?: string,
-  role?: string,
-  default_value?: string,
+  name: string;
+  type?: string;
+  role?: string;
+  default_value?: string;
 }
 
 interface AfterViewInit {
-  ngAfterViewInit(): void
+  ngAfterViewInit(): void;
 }
 
 @Component({
-  selector: "docuseal-builder",
+  selector: 'docuseal-builder',
   standalone: true,
-  template: "",
-  styles: []
+  template: '',
+  styles: [],
 })
-
 export class DocusealBuilderComponent implements AfterViewInit {
-  @Input() token: string = ""
-  @Input() host: string = "cdn.docuseal.co"
-  @Input() language: string = "en"
-  @Input() preview: boolean = false
-  @Input() autosave: boolean = true
-  @Input() withRecipientsButton: boolean = true
-  @Input() withDocumentsList: boolean = true
-  @Input() withFieldsList: boolean = true
-  @Input() withSendButton: boolean = true
-  @Input() withTitle: boolean = true
-  @Input() onlyDefinedFields: boolean = false
-  @Input() withSignYourselfButton: boolean = true
-  @Input() withUploadButton: boolean = true
-  @Input() roles: string[] = []
-  @Input() fields: DocusealField[] = []
-  @Input() i18n: object = {}
-  @Input() fieldTypes: string[] = []
-  @Input() drawFieldType: string = "text"
-  @Input() customButton: { title: string, url: string } = { title: "", url: "" }
-  @Input() backgroundColor: string = ""
-  @Input() onLoad: (detail: any) => void = () => {}
-  @Input() onUpload: (detail: any) => void = () => {}
-  @Input() onSend: (detail: any) => void = () => {}
-  @Input() onSave: (detail: any) => void = () => {}
-  @Input() sendButtonText: string = ""
-  @Input() saveButtonText: string = ""
-  @Input() customCss: string = ""
+  @Input() token: string = '';
+  @Input() host: string = 'cdn.docuseal.co';
+  @Input() language: string = 'en';
+  @Input() preview: boolean = false;
+  @Input() autosave: boolean = true;
+  @Input() withRecipientsButton: boolean = true;
+  @Input() withDocumentsList: boolean = true;
+  @Input() withFieldsList: boolean = true;
+  @Input() withSendButton: boolean = true;
+  @Input() withTitle: boolean = true;
+  @Input() onlyDefinedFields: boolean = false;
+  @Input() withSignYourselfButton: boolean = true;
+  @Input() withUploadButton: boolean = true;
+  @Input() roles: string[] = [];
+  @Input() fields: DocusealField[] = [];
+  @Input() i18n: object = {};
+  @Input() fieldTypes: string[] = [];
+  @Input() drawFieldType: string = 'text';
+  @Input() customButton: { title: string; url: string } = {
+    title: '',
+    url: '',
+  };
+  @Input() backgroundColor: string = '';
+  @Input() sendButtonText: string = '';
+  @Input() saveButtonText: string = '';
+  @Input() customCss: string = '';
 
-  @HostBinding("attr.data-token") get dataToken(): string { return this.token }
-  @HostBinding("attr.data-preview") get dataPreview(): boolean { return this.preview }
-  @HostBinding("attr.data-language") get dataLanguage(): string { return this.language }
-  @HostBinding("attr.data-autosave") get dataAutosave(): boolean { return this.autosave }
-  @HostBinding("attr.data-send-button-text") get dataSendButtonText(): string { return this.sendButtonText }
-  @HostBinding("attr.data-save-button-text") get dataSaveButtonText(): string { return this.saveButtonText }
-  @HostBinding("attr.data-roles") get dataRoles(): string { return this.roles.join(',') }
-  @HostBinding("attr.data-field-types") get dataFieldTypes(): string { return this.fieldTypes.join(',') }
-  @HostBinding("attr.data-draw-field-type") get dataDrawFieldType(): string { return this.drawFieldType }
-  @HostBinding("attr.data-fields") get dataFields(): string { return JSON.stringify(this.fields) }
-  @HostBinding("attr.data-i18n") get dataI18n(): string { return JSON.stringify(this.i18n) }
-  @HostBinding("attr.data-custom-button-title") get dataCustomButtonTitle(): string { return this.customButton.title }
-  @HostBinding("attr.data-custom-button-url") get dataCustomButtonUrl(): string { return this.customButton.url }
-  @HostBinding("attr.data-with-recipients-button") get dataWithRecipientsButton(): boolean { return this.withRecipientsButton }
-  @HostBinding("attr.data-with-send-button") get dataWithSendButton(): boolean { return this.withSendButton }
-  @HostBinding("attr.data-with-documents-list") get dataWithDocumentsList(): boolean { return this.withDocumentsList }
-  @HostBinding("attr.data-with-fields-list") get dataWithFieldsList(): boolean { return this.withFieldsList }
-  @HostBinding("attr.data-with-title") get dataWithTitle(): boolean { return this.withTitle }
-  @HostBinding("attr.data-only-defined-fields") get dataOnlyDefinedFields(): boolean { return this.onlyDefinedFields }
-  @HostBinding("attr.data-with-upload-button") get dataWithUploadButton(): boolean { return this.withUploadButton }
-  @HostBinding("attr.data-with-sign-yourself-button") get dataWithSignYourselfButton(): boolean { return this.withSignYourselfButton }
-  @HostBinding("attr.data-background-color") get dataBackgroundColor(): string { return this.backgroundColor }
-  @HostBinding("attr.data-custom-css") get dataCustomCss(): string { return this.customCss }
+  @Output() onLoad = new EventEmitter<any>();
+  @Output() onUpload = new EventEmitter<any>();
+  @Output() onSend = new EventEmitter<any>();
+  @Output() onSave = new EventEmitter<any>();
+
+  @HostBinding('attr.data-token') get dataToken(): string {
+    return this.token;
+  }
+  @HostBinding('attr.data-preview') get dataPreview(): boolean {
+    return this.preview;
+  }
+  @HostBinding('attr.data-language') get dataLanguage(): string {
+    return this.language;
+  }
+  @HostBinding('attr.data-autosave') get dataAutosave(): boolean {
+    return this.autosave;
+  }
+  @HostBinding('attr.data-send-button-text') get dataSendButtonText(): string {
+    return this.sendButtonText;
+  }
+  @HostBinding('attr.data-save-button-text') get dataSaveButtonText(): string {
+    return this.saveButtonText;
+  }
+  @HostBinding('attr.data-roles') get dataRoles(): string {
+    return this.roles.join(',');
+  }
+  @HostBinding('attr.data-field-types') get dataFieldTypes(): string {
+    return this.fieldTypes.join(',');
+  }
+  @HostBinding('attr.data-draw-field-type') get dataDrawFieldType(): string {
+    return this.drawFieldType;
+  }
+  @HostBinding('attr.data-fields') get dataFields(): string {
+    return JSON.stringify(this.fields);
+  }
+  @HostBinding('attr.data-i18n') get dataI18n(): string {
+    return JSON.stringify(this.i18n);
+  }
+  @HostBinding('attr.data-custom-button-title')
+  get dataCustomButtonTitle(): string {
+    return this.customButton.title;
+  }
+  @HostBinding('attr.data-custom-button-url')
+  get dataCustomButtonUrl(): string {
+    return this.customButton.url;
+  }
+  @HostBinding('attr.data-with-recipients-button')
+  get dataWithRecipientsButton(): boolean {
+    return this.withRecipientsButton;
+  }
+  @HostBinding('attr.data-with-send-button') get dataWithSendButton(): boolean {
+    return this.withSendButton;
+  }
+  @HostBinding('attr.data-with-documents-list')
+  get dataWithDocumentsList(): boolean {
+    return this.withDocumentsList;
+  }
+  @HostBinding('attr.data-with-fields-list') get dataWithFieldsList(): boolean {
+    return this.withFieldsList;
+  }
+  @HostBinding('attr.data-with-title') get dataWithTitle(): boolean {
+    return this.withTitle;
+  }
+  @HostBinding('attr.data-only-defined-fields')
+  get dataOnlyDefinedFields(): boolean {
+    return this.onlyDefinedFields;
+  }
+  @HostBinding('attr.data-with-upload-button')
+  get dataWithUploadButton(): boolean {
+    return this.withUploadButton;
+  }
+  @HostBinding('attr.data-with-sign-yourself-button')
+  get dataWithSignYourselfButton(): boolean {
+    return this.withSignYourselfButton;
+  }
+  @HostBinding('attr.data-background-color') get dataBackgroundColor(): string {
+    return this.backgroundColor;
+  }
+  @HostBinding('attr.data-custom-css') get dataCustomCss(): string {
+    return this.customCss;
+  }
 
   ngAfterViewInit(): void {
-    this.loadScript()
+    this.loadScript();
   }
 
   @HostListener('send', ['$event'])
   onSendEvent(event: CustomEvent): void {
     if (this.onSend) {
-      this.onSend(event.detail)
+      this.onSend.emit(event.detail);
     }
   }
 
   @HostListener('load', ['$event'])
   onLoadEvent(event: CustomEvent): void {
     if (this.onLoad) {
-      this.onLoad(event.detail)
+      this.onLoad.emit(event.detail);
     }
   }
 
   @HostListener('upload', ['$event'])
   onUploadEvent(event: CustomEvent): void {
     if (this.onUpload) {
-      this.onUpload(event.detail)
+      this.onUpload.emit(event.detail);
     }
   }
 
   @HostListener('save', ['$event'])
   onSaveEvent(event: CustomEvent): void {
     if (this.onSave) {
-      this.onSave(event.detail)
+      this.onSave.emit(event.detail);
     }
   }
 
   loadScript(): void {
-    const scriptId = 'docuseal-builder-script'
-    const scriptSrc = `https://${this.host}/js/builder.js`
-    const script = document.createElement("script")
+    const scriptId = 'docuseal-builder-script';
+    const scriptSrc = `https://${this.host}/js/builder.js`;
+    const script = document.createElement('script');
 
-    script.id = scriptId
-    script.async = true
-    script.src = scriptSrc
+    script.id = scriptId;
+    script.async = true;
+    script.src = scriptSrc;
 
-    document.head.appendChild(script)
+    document.head.appendChild(script);
   }
 }
--- src/form.component.ts
+++ src/form.component.ts
@@ -1,120 +1,194 @@
-import { Component, Input, HostListener, HostBinding } from "@angular/core"
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
 
 interface DocusealField {
-  name: string,
-  title?: string,
-  type?: string,
-  position?: number,
-  required?: boolean,
+  name: string;
+  title?: string;
+  type?: string;
+  position?: number;
+  required?: boolean;
 }
 
 interface AfterViewInit {
-  ngAfterViewInit(): void
+  ngAfterViewInit(): void;
 }
 
 @Component({
-  selector: "docuseal-form",
+  selector: 'docuseal-form',
   standalone: true,
-  template: "",
-  styles: []
+  template: '',
+  styles: [],
 })
-
 export class DocusealFormComponent implements AfterViewInit {
-  @Input() src: string = ""
-  @Input() host: string = "cdn.docuseal.co"
-  @Input() role: string = ""
-  @Input() submitter: string = "" // Backward compatibility
-  @Input() expand: boolean = true
-  @Input() preview: boolean = false
-  @Input() email: string = ""
-  @Input() applicationKey: string = ""
-  @Input() externalId: string = ""
-  @Input() backgroundColor: string = ""
-  @Input() logo: string = ""
-  @Input() language: string = ""
-  @Input() completedRedirectUrl: string = ""
-  @Input() completedButton: { title: string, url: string } = { title: "", url: "" }
-  @Input() goToLast: boolean = true
-  @Input() skipFields: boolean = false
-  @Input() withTitle: boolean = true
-  @Input() withFieldNames: boolean = true
-  @Input() withDownloadButton: boolean = true
-  @Input() withSendCopyButton: boolean = true
-  @Input() allowToResubmit: boolean = true
-  @Input() allowTypedSignature: boolean = true
-  @Input() sendCopyEmail: boolean | null = null
-  @Input() values: object = {}
-  @Input() metadata: object = {}
-  @Input() i18n: object = {}
-  @Input() fields: DocusealField[] = []
-  @Input() readonlyFields: string[] = []
-  @Input() onComplete: (detail: any) => void = () => {}
-  @Input() onInit: (detail: any) => void = () => {}
-  @Input() onLoad: (detail: any) => void = () => {}
-  @Input() customCss: string = ""
+  @Input() src: string = '';
+  @Input() host: string = 'cdn.docuseal.co';
+  @Input() role: string = '';
+  @Input() submitter: string = ''; // Backward compatibility
+  @Input() expand: boolean = true;
+  @Input() preview: boolean = false;
+  @Input() email: string = '';
+  @Input() applicationKey: string = '';
+  @Input() externalId: string = '';
+  @Input() backgroundColor: string = '';
+  @Input() logo: string = '';
+  @Input() language: string = '';
+  @Input() completedRedirectUrl: string = '';
+  @Input() completedButton: { title: string; url: string } = {
+    title: '',
+    url: '',
+  };
+  @Input() goToLast: boolean = true;
+  @Input() skipFields: boolean = false;
+  @Input() withTitle: boolean = true;
+  @Input() withFieldNames: boolean = true;
+  @Input() withDownloadButton: boolean = true;
+  @Input() withSendCopyButton: boolean = true;
+  @Input() allowToResubmit: boolean = true;
+  @Input() allowTypedSignature: boolean = true;
+  @Input() sendCopyEmail: boolean | null = null;
+  @Input() values: object = {};
+  @Input() metadata: object = {};
+  @Input() i18n: object = {};
+  @Input() fields: DocusealField[] = [];
+  @Input() readonlyFields: string[] = [];
 
-  @HostBinding("attr.data-src") get dataSrc(): string { return this.src }
-  @HostBinding("attr.data-email") get dataEmail(): string { return this.email }
-  @HostBinding("attr.data-role") get dataRole(): string { return this.role || this.submitter }
-  @HostBinding("attr.data-external-id") get dataExternalId(): string { return this.externalId || this.applicationKey }
-  @HostBinding("attr.data-expand") get dataExpand(): boolean { return this.expand }
-  @HostBinding("attr.data-preview") get dataPreview(): boolean { return this.preview }
-  @HostBinding("attr.data-go-to-last") get dataGoToLast(): boolean { return this.goToLast }
-  @HostBinding("attr.data-skip-fields") get dataSkipFields(): boolean { return this.skipFields }
-  @HostBinding("attr.data-send-copy-email") get dataSendCopyEmail(): boolean | null { return this.sendCopyEmail }
-  @HostBinding("attr.data-with-title") get dataWithTitle(): boolean { return this.withTitle }
-  @HostBinding("attr.data-logo") get dataLogo(): string { return this.logo }
-  @HostBinding("attr.data-language") get dataLanguage(): string { return this.language }
-  @HostBinding("attr.data-with-field-names") get dataWithFieldNames(): boolean { return this.withFieldNames }
-  @HostBinding("attr.data-with-download-button") get dataWithDownloadButton(): boolean { return this.withDownloadButton }
-  @HostBinding("attr.data-allow-to-resubmit") get dataAllowToResubmit(): boolean { return this.allowToResubmit }
-  @HostBinding("attr.data-allow-typed-signature") get dataAllowTypedSignature(): boolean { return this.allowTypedSignature }
-  @HostBinding("attr.data-completed-redirect-url") get dataCompletedRedirectUrl(): string { return this.completedRedirectUrl }
-  @HostBinding("attr.data-with-send-copy-button") get dataWithSendCopyButton(): boolean { return this.withSendCopyButton }
-  @HostBinding("attr.data-values") get dataValues(): string { return JSON.stringify(this.values) }
-  @HostBinding("attr.data-metadata") get dataMetadata(): string { return JSON.stringify(this.metadata) }
-  @HostBinding("attr.data-fields") get dataFields(): string { return JSON.stringify(this.fields) }
-  @HostBinding("attr.data-i18n") get dataI18n(): string { return JSON.stringify(this.i18n) }
-  @HostBinding("attr.data-readonly-fields") get dataReadonlyFields(): string { return this.readonlyFields.join(',') }
-  @HostBinding("attr.data-completed-button-title") get dataCompletedButtonTitle(): string { return this.completedButton.title }
-  @HostBinding("attr.data-completed-button-url") get dataCompletedButtonUrl(): string { return this.completedButton.url }
-  @HostBinding("attr.data-background-color") get dataBackgroundColor(): string { return this.backgroundColor }
-  @HostBinding("attr.data-custom-css") get dataCustomCss(): string { return this.customCss }
+  @Input() customCss: string = '';
+
+  @Output() onComplete = new EventEmitter<any>();
+  @Output() onLoad = new EventEmitter<any>();
+  @Output() onInit = new EventEmitter<any>();
+
+  @HostBinding('attr.data-src') get dataSrc(): string {
+    return this.src;
+  }
+  @HostBinding('attr.data-email') get dataEmail(): string {
+    return this.email;
+  }
+  @HostBinding('attr.data-role') get dataRole(): string {
+    return this.role || this.submitter;
+  }
+  @HostBinding('attr.data-external-id') get dataExternalId(): string {
+    return this.externalId || this.applicationKey;
+  }
+  @HostBinding('attr.data-expand') get dataExpand(): boolean {
+    return this.expand;
+  }
+  @HostBinding('attr.data-preview') get dataPreview(): boolean {
+    return this.preview;
+  }
+  @HostBinding('attr.data-go-to-last') get dataGoToLast(): boolean {
+    return this.goToLast;
+  }
+  @HostBinding('attr.data-skip-fields') get dataSkipFields(): boolean {
+    return this.skipFields;
+  }
+  @HostBinding('attr.data-send-copy-email') get dataSendCopyEmail():
+    | boolean
+    | null {
+    return this.sendCopyEmail;
+  }
+  @HostBinding('attr.data-with-title') get dataWithTitle(): boolean {
+    return this.withTitle;
+  }
+  @HostBinding('attr.data-logo') get dataLogo(): string {
+    return this.logo;
+  }
+  @HostBinding('attr.data-language') get dataLanguage(): string {
+    return this.language;
+  }
+  @HostBinding('attr.data-with-field-names') get dataWithFieldNames(): boolean {
+    return this.withFieldNames;
+  }
+  @HostBinding('attr.data-with-download-button')
+  get dataWithDownloadButton(): boolean {
+    return this.withDownloadButton;
+  }
+  @HostBinding('attr.data-allow-to-resubmit')
+  get dataAllowToResubmit(): boolean {
+    return this.allowToResubmit;
+  }
+  @HostBinding('attr.data-allow-typed-signature')
+  get dataAllowTypedSignature(): boolean {
+    return this.allowTypedSignature;
+  }
+  @HostBinding('attr.data-completed-redirect-url')
+  get dataCompletedRedirectUrl(): string {
+    return this.completedRedirectUrl;
+  }
+  @HostBinding('attr.data-with-send-copy-button')
+  get dataWithSendCopyButton(): boolean {
+    return this.withSendCopyButton;
+  }
+  @HostBinding('attr.data-values') get dataValues(): string {
+    return JSON.stringify(this.values);
+  }
+  @HostBinding('attr.data-metadata') get dataMetadata(): string {
+    return JSON.stringify(this.metadata);
+  }
+  @HostBinding('attr.data-fields') get dataFields(): string {
+    return JSON.stringify(this.fields);
+  }
+  @HostBinding('attr.data-i18n') get dataI18n(): string {
+    return JSON.stringify(this.i18n);
+  }
+  @HostBinding('attr.data-readonly-fields') get dataReadonlyFields(): string {
+    return this.readonlyFields.join(',');
+  }
+  @HostBinding('attr.data-completed-button-title')
+  get dataCompletedButtonTitle(): string {
+    return this.completedButton.title;
+  }
+  @HostBinding('attr.data-completed-button-url')
+  get dataCompletedButtonUrl(): string {
+    return this.completedButton.url;
+  }
+  @HostBinding('attr.data-background-color') get dataBackgroundColor(): string {
+    return this.backgroundColor;
+  }
+  @HostBinding('attr.data-custom-css') get dataCustomCss(): string {
+    return this.customCss;
+  }
 
   ngAfterViewInit(): void {
-    this.loadScript()
+    this.loadScript();
   }
 
   @HostListener('completed', ['$event'])
   onCompleteEvent(event: CustomEvent): void {
     if (this.onComplete) {
-      this.onComplete(event.detail)
+      this.onComplete.emit(event.detail);
     }
   }
 
   @HostListener('init', ['$event'])
   onInitEvent(event: CustomEvent): void {
     if (this.onInit) {
-      this.onInit(event.detail)
+      this.onInit.emit(event.detail);
     }
   }
 
   @HostListener('load', ['$event'])
   onLoadEvent(event: CustomEvent): void {
     if (this.onLoad) {
-      this.onLoad(event.detail)
+      this.onLoad.emit(event.detail);
     }
   }
 
   loadScript(): void {
-    const scriptId = "docuseal-form-script"
-    const scriptSrc = `https://${this.host}/js/form.js`
-    const script = document.createElement("script")
+    const scriptId = 'docuseal-form-script';
+    const scriptSrc = `https://${this.host}/js/form.js`;
+    const script = document.createElement('script');
 
-    script.id = scriptId
-    script.async = true
-    script.src = scriptSrc
+    script.id = scriptId;
+    script.async = true;
+    script.src = scriptSrc;
 
-    document.head.appendChild(script)
+    document.head.appendChild(script);
   }
 }
